900daf84978633cb085e9da88e75ddd2402c0514
Dayvson Silva <userddssilva@gmail.com>
2021-03-06 13:12:55 -0400

    feat(): adiciona código base

---
+++ Assets/scripts/ShowText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowText : MonoBehaviour
+{
+    public Text word;
+    public Button btnChangeWord;
+
+    void Start()
+    {
+        word.text = "Café";
+        btnChangeWord.onClick.AddListener(TaskOnClick);
+    }
+
+    void Update()
+    {
+    }
+
+    private void TaskOnClick()
+    {
+        if (word.text == "Café")
+        {
+            word.text = "Leite";
+        } 
+        else
+        {
+            word.text = "Café";
+        }
+    }
+}
