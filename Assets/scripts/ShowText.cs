981173e3d312d0149067796c247351796e02e3bf
Dayvson Silva <userddssilva@gmail.com>
2021-03-07 15:08:20 -0400

    feat(): create world random choice by click a button

--- Assets/scripts/ShowText.cs
+++ Assets/scripts/ShowText.cs
@@ -2,31 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
+
+using System;
+using System.IO;
+
+using System.Threading.Tasks;
 
 public class ShowText : MonoBehaviour
 {
     public Text word;
     public Button btnChangeWord;
+    private static List<string> wordList;
 
     void Start()
     {
-        word.text = "Café";
-        btnChangeWord.onClick.AddListener(TaskOnClick);
+        wordList = new List<string>();
+        
+        async_read_file();
+
+        choiceRandomWorld;
+
+        btnChangeWord.onClick.AddListener(choiceRandomWorld);
     }
 
     void Update()
     {
     }
 
-    private void TaskOnClick()
+    private void choiceRandomWorld()
     {
-        if (word.text == "Café")
+        if (wordList.Count > 0)
         {
-            word.text = "Leite";
-        } 
-        else
+            System.Random rd = new System.Random();
+            int index = rd.Next(wordList.Count);
+            word.text = wordList[index];
+            wordList.RemoveAt(index);
+        }
+    }
+
+
+    static async Task async_read_file()
+    {
+        await ReadAndDisplayFilesAsync();
+    }
+
+    static async Task ReadAndDisplayFilesAsync()
+    {
+        String filename = Application.dataPath + "/resources/pt_BR_dic_1.txt";
+
+        using (StreamReader sr = new StreamReader(filename))
         {
-            word.text = "Café";
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                wordList.Add(line);
+                // Debug.Log(line);
+            }
         }
     }
 }
